b25e1cecf763f9ca479fa83b748612b2d91b7755
Vladimir <azuresplash@gmail.com>
2020-02-01 17:16:44 +1100

    Removed most schema types and queries

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -4,71 +4,17 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     return cooked;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var fruit_service_1 = require("./fruit-service");
 var apollo_server_1 = require("apollo-server");
 var chat_service_1 = require("./chat-service");
-var graphql_1 = require("graphql");
-var typeDefs = apollo_server_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type Query {\n    fruits: [Fruit!]!\n    messages: [Message!]!\n    users: [User!]!\n  }\n\n  type Mutation {\n    addFruit(input: AddFruitInput!): Fruit!\n    login(name: String!): User!\n    addMessage(senderId: ID, text: String!): Message!\n  }\n\n  type Subscription {\n    addedMessage: Message!\n    addedUser: User!\n  }\n\n  input AddFruitInput {\n    name: String!\n  }\n\n  type Message {\n    id: ID!\n    senderId: ID\n    creationDate: Date!\n    text: String!\n  }\n\n  type User {\n    id: ID!\n    name: String!\n    avatarUrl: String!\n  }\n\n  type Fruit {\n    id: ID!\n    name: String!\n  }\n\n  scalar Date\n"], ["\n  type Query {\n    fruits: [Fruit!]!\n    messages: [Message!]!\n    users: [User!]!\n  }\n\n  type Mutation {\n    addFruit(input: AddFruitInput!): Fruit!\n    login(name: String!): User!\n    addMessage(senderId: ID, text: String!): Message!\n  }\n\n  type Subscription {\n    addedMessage: Message!\n    addedUser: User!\n  }\n\n  input AddFruitInput {\n    name: String!\n  }\n\n  type Message {\n    id: ID!\n    senderId: ID\n    creationDate: Date!\n    text: String!\n  }\n\n  type User {\n    id: ID!\n    name: String!\n    avatarUrl: String!\n  }\n\n  type Fruit {\n    id: ID!\n    name: String!\n  }\n\n  scalar Date\n"])));
+var typeDefs = apollo_server_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type Query {\n    fruits: [Fruit!]!\n  }\n\n  type Fruit {\n    id: ID!\n    name: String!\n  }\n"], ["\n  type Query {\n    fruits: [Fruit!]!\n  }\n\n  type Fruit {\n    id: ID!\n    name: String!\n  }\n"])));
 var pubSub = new apollo_server_1.PubSub();
-var dateType = new graphql_1.GraphQLScalarType({
-    name: "Date",
-    description: "Date",
-    serialize: function (value) {
-        return value.getTime();
-    },
-    parseValue: function (value) {
-        return new Date(value);
-    },
-    parseLiteral: function (ast) {
-        if (ast.kind === graphql_1.Kind.INT) {
-            return ast.value;
-        }
-        return null;
-    }
-});
 var resolvers = {
-    Query: {
-        fruits: function (parent, args, context) { return context.fruitService.getAll(); },
-        messages: function (parent, args, context) { return context.chatService.getAllMessages(); },
-        users: function (parent, args, context) { return context.chatService.getAllUsers(); }
-    },
-    Mutation: {
-        addFruit: function (parent, args, context) { return context.fruitService.add(args.input); },
-        login: function (parent, args, context) {
-            var addedUser = context.chatService.addUser({ name: args.name });
-            console.log("user!", addedUser);
-            if (addedUser) {
-                pubSub.publish("addedUser", { addedUser: addedUser });
-            }
-            return addedUser;
-        },
-        addMessage: function (parent, args, context) {
-            var addedMessage = context.chatService.addMessage({ senderId: args.senderId, text: args.text });
-            if (addedMessage) {
-                pubSub.publish("addedMessage", { addedMessage: addedMessage });
-            }
-            return addedMessage;
-        }
-    },
-    User: {
-        avatarUrl: function (user) {
-            return "https://robohash.org/" + user.name + ".png";
-        }
-    },
-    Date: dateType,
-    Subscription: {
-        addedUser: {
-            subscribe: function () { return pubSub.asyncIterator("addedUser"); }
-        },
-        addedMessage: {
-            subscribe: function () { return pubSub.asyncIterator("addedMessage"); }
-        }
-    }
+    Query: {}
 };
 var server = new apollo_server_1.ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers,
-    context: { fruitService: new fruit_service_1.FruitService(), chatService: new chat_service_1.ChatService() },
+    context: { chatService: new chat_service_1.ChatService() },
     cors: true
 });
 server.listen().then(function (_a) {
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,5 @@
-import { FruitService } from "./fruit-service";
 import { ApolloServer, gql, PubSub } from "apollo-server";
 import { ChatService } from "./chat-service";
-import { GraphQLScalarType, Kind } from "graphql";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -9,110 +7,25 @@ import { GraphQLScalarType, Kind } from "graphql";
 const typeDefs = gql`
   type Query {
     fruits: [Fruit!]!
-    messages: [Message!]!
-    users: [User!]!
-  }
-
-  type Mutation {
-    addFruit(input: AddFruitInput!): Fruit!
-    login(name: String!): User!
-    addMessage(senderId: ID, text: String!): Message!
-  }
-
-  type Subscription {
-    addedMessage: Message!
-    addedUser: User!
-  }
-
-  input AddFruitInput {
-    name: String!
-  }
-
-  type Message {
-    id: ID!
-    senderId: ID
-    creationDate: Date!
-    text: String!
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    avatarUrl: String!
   }
 
   type Fruit {
     id: ID!
     name: String!
   }
-
-  scalar Date
 `;
 
 const pubSub = new PubSub();
 
-const dateType = new GraphQLScalarType({
-  name: "Date",
-  description: "Date",
-  serialize(value: Date): number {
-    return value.getTime();
-  },
-  parseValue(value: number): Date {
-    return new Date(value);
-  },
-  parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      return ast.value;
-    }
-    return null;
-  }
-});
-
 const resolvers = {
   Query: {
-    fruits: (parent, args, context) => context.fruitService.getAll(),
-    messages: (parent, args, context) => context.chatService.getAllMessages(),
-    users: (parent, args, context) => context.chatService.getAllUsers()
-  },
-  Mutation: {
-    addFruit: (parent, args, context) => context.fruitService.add(args.input),
-    login: (parent, args, context) => {
-      const addedUser = context.chatService.addUser({ name: args.name });
-      console.log("user!", addedUser);
-      if (addedUser) {
-        pubSub.publish("addedUser", { addedUser });
-      }
-      return addedUser;
-    },
-    addMessage: (parent, args, context) => {
-      const addedMessage = context.chatService.addMessage({ senderId: args.senderId, text: args.text });
-      if (addedMessage) {
-        pubSub.publish("addedMessage", { addedMessage });
-      }
-
-      return addedMessage;
-    }
-  },
-  User: {
-    avatarUrl(user) {
-      return `https://robohash.org/${user.name}.png`;
-    }
-  },
-  Date: dateType,
-  Subscription: {
-    addedUser: {
-      subscribe: () => pubSub.asyncIterator("addedUser")
-    },
-    addedMessage: {
-      subscribe: () => pubSub.asyncIterator("addedMessage")
-    }
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: { fruitService: new FruitService(), chatService: new ChatService() },
+  context: { chatService: new ChatService() },
   cors: true
 });
 
